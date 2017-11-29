be3ad0fc47244c557a71f495a8a1e482bfd37261
hkgoldenmra <hkgolden2003a@gmail.com>
2017-11-30 02:14:33 +0800

    import json string interactively

--- checkpoints/bio-hazard-2-leon-a.js
+++
@@ -1,5 +0,0 @@
-var checkpointBH2LeonA = [
-	{"title": "G0", "hours": 0, "minutes": 28, "seconds": 5, "milliseconds": 0},
-	{"title": "G2", "hours": 0, "minutes": 39, "seconds": 29, "milliseconds": 0},
-	{"title": "G4", "hours": 0, "minutes": 50, "seconds": 34, "milliseconds": 0}
-];
--- functions.js
+++ functions.js
@@ -1,13 +1,13 @@
-var showMilliseconds = true;
 var showHours = true;
+var showMilliseconds = true;
 var fields = window.location.href.split('?');
 fields = fields[1].split('&');
 for (var i in fields){
 	var params = fields[i].split('=');
-	if (params[0] == 'showms'){
-		showMilliseconds = params[1] == 1;
-	} else if (params[0] == 'showh'){
+	if (params[0] == 'showh'){
 		showHours = params[1] == 1;
+	} else if (params[0] == 'showms'){
+		showMilliseconds = params[1] == 1;
 	}
 }
 
@@ -15,60 +15,72 @@ var timerId = null;
 var frames = 0;
 var checkpointIndex = 0;
 
-function loadCheckpoint(){
+function loadCheckpoint(jsonString){
+	var error = document.getElementById('error');
+	error.innerHTML = '';
 	var table = document.getElementById('checkpointTable');
 	var tbody = table.getElementsByTagName('tbody');
 	tbody = tbody[0];
 	while (tbody.rows.length > 0){
 		tbody.deleteRow(0);
 	}
-	for (var i in checkpoints){
-		var checkpoint = checkpoints[i];
-		var tr = tbody.insertRow(-1);
-		tr.id = 'currentTimer' + i;
-//
-		var td = tr.insertCell(-1);
-		td.align = 'right';
-		td.innerHTML = checkpoint.title;
-//
-		td = tr.insertCell(-1);
-		td.align = 'right';
-		var seconds = checkpoint.seconds;
-		if (seconds == undefined){
-			seconds = 0;
-		}
-		var minutes = checkpoint.minutes;
-		if (minutes == undefined){
-			minutes = 0;
-		}
-		var secondString = padZero(seconds, 2);
-		var minuteString = padZero(minutes, 2);
-		var string = minuteString + ':' + secondString;
-		if (showHours){
-			var hours = checkpoint.hours;
-			if (hours == undefined){
-				hours = 0;
+	try{
+		var checkpoints = JSON.parse(document.getElementById('json').value);
+		for (var i in checkpoints){
+			var checkpoint = checkpoints[i];
+			var tr = tbody.insertRow(-1);
+			tr.id = 'currentTimer' + i;
+	//
+			var td = tr.insertCell(-1);
+			td.align = 'right';
+			var title = checkpoint.title;
+			if (title == undefined){
+				td.innerHTML = i;
+			} else {
+				td.innerHTML = checkpoint.title;
 			}
-			var hourString = padZero(hours, 2);
-			string = hourString + ':' + string;
-		}
-		if (showMilliseconds){
-			var milliseconds = checkpoint.milliseconds;
-			if (milliseconds == undefined){
-				milliseconds = 0;
+	//
+			td = tr.insertCell(-1);
+			td.align = 'right';
+			var seconds = checkpoint.s;
+			if (seconds == undefined){
+				seconds = 0;
 			}
-			var millisecondString = padZero(milliseconds, 3);
-			string += '.' + millisecondString;
+			var minutes = checkpoint.m;
+			if (minutes == undefined){
+				minutes = 0;
+			}
+			var secondString = padZero(seconds, 2);
+			var minuteString = padZero(minutes, 2);
+			var string = minuteString + ':' + secondString;
+			if (showHours){
+				var hours = checkpoint.h;
+				if (hours == undefined){
+					hours = 0;
+				}
+				var hourString = padZero(hours, 2);
+				string = hourString + ':' + string;
+			}
+			if (showMilliseconds){
+				var milliseconds = checkpoint.ms;
+				if (milliseconds == undefined){
+					milliseconds = 0;
+				}
+				var millisecondString = padZero(milliseconds, 3);
+				string += '.' + millisecondString;
+			}
+			td.innerHTML = string;
+	//
+			td = tr.insertCell(-1);
+			td.align = 'right';
+			td.id = 'currentTime' + i;
+	//
+			td = tr.insertCell(-1);
+			td.align = 'right';
+			td.id = 'comparedTime' + i;
 		}
-		td.innerHTML = string;
-//
-		td = tr.insertCell(-1);
-		td.align = 'right';
-		td.id = 'currentTime' + i;
-//
-		td = tr.insertCell(-1);
-		td.align = 'right';
-		td.id = 'comparedTime' + i;
+	} catch (ex){
+		error.innerHTML = ex.toString();
 	}
 }
 
@@ -106,17 +118,19 @@ function resetTimer(){
 
 function updateStatus(){
 	if (timerId != null){
+		var checkpoints = JSON.parse(document.getElementById('json').value);
 		frames++;
 //
-		var currentMilliseconds = originalMilliseconds = Math.floor(frames * 1000 / 60);
+		var currentMilliseconds = Math.floor(frames * 1000 / 60);
 		var currentSeconds = Math.floor(currentMilliseconds / 1000);
 		var currentMinutes = Math.floor(currentSeconds / 60);
 		var currentHours = Math.floor(currentMinutes / 24);
+		var originalMilliseconds = currentMilliseconds;
 //
 		currentMilliseconds %= 1000;
 		currentSeconds %= 60;
 		currentMinutes %= 60;
-		currentHours %= 24;
+//		currentHours %= 24;
 //
 		var currentSecondString = padZero(currentSeconds, 2);
 		var currentMinuteString = padZero(currentMinutes, 2);
@@ -135,44 +149,41 @@ function updateStatus(){
 			var element = document.getElementById('currentTimer' + i);
 			element.style.backgroundColor = '#000000';
 			if (i == checkpointIndex){
-				element.style.backgroundColor = '#222222';
+				element.style.backgroundColor = '#666666';
 			}
 		}
 		for (var i = checkpointIndex; i < checkpoints.length; i++){
 			var checkpoint = checkpoints[i];
-			var targetMilliseconds = 0;
-			var hours = checkpoint.hours;
-			if (hours == undefined){
-				hours = 0;
-			}
-			var minutes = checkpoint.minutes;
-			if (minutes == undefined){
-				minutes = 0;
-			}
-			var seconds = checkpoint.seconds;
-			if (seconds == undefined){
-				seconds = 0;
-			}
-			var milliseconds = checkpoint.milliseconds;
+			var milliseconds = checkpoint.ms;
 			if (milliseconds == undefined){
 				milliseconds = 0;
 			}
-			targetMilliseconds += hours * 60 * 60 * 1000;
-			targetMilliseconds += minutes * 60 * 1000;
+			var seconds = checkpoint.s;
+			if (seconds == undefined){
+				seconds = 0;
+			}
+			var minutes = checkpoint.m;
+			if (minutes == undefined){
+				minutes = 0;
+			}
+			var hours = checkpoint.h;
+			if (hours == undefined){
+				hours = 0;
+			}
+			var targetMilliseconds = milliseconds;
 			targetMilliseconds += seconds * 1000;
-			targetMilliseconds += milliseconds;
+			targetMilliseconds += minutes * 60 * 1000;
+			targetMilliseconds += hours * 60 * 60 * 1000;
 //
 			var comparedMilliseconds = targetMilliseconds - originalMilliseconds;
 			var isNegative = comparedMilliseconds < 0;
 			comparedMilliseconds = Math.abs(comparedMilliseconds);
+			var comparedHours = Math.floor(comparedMilliseconds / 3600000);
+			comparedMilliseconds %= 3600000;
+			var comparedMinutes = Math.floor(comparedMilliseconds / 60000);
+			comparedMilliseconds %= 60000;
 			var comparedseconds = Math.floor(comparedMilliseconds / 1000);
-			var comparedMinutes = Math.floor(comparedseconds / 60);
-			var comparedHours = Math.floor(comparedMinutes / 24);
-//
 			comparedMilliseconds %= 1000;
-			comparedseconds %= 60;
-			comparedMinutes %= 60;
-			comparedHours %= 24;
 //
 			var comparedSecondString = padZero(comparedseconds, 2);
 			var comparedMinuteString = padZero(comparedMinutes, 2);
@@ -200,6 +211,7 @@ function submitCheckpointOnBody(event){
 }
 
 function submitCheckpoint(){
+	var checkpoints = JSON.parse(document.getElementById('json').value);
 	if (checkpointIndex < checkpoints.length){
 		checkpointIndex++;
 	} else {
